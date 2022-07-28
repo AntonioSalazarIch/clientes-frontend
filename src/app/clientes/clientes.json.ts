06270b27b39d8ea09bc54091b07223770e5a0d55
antonio.salazar <antonio.salazar@et.bo>
2022-07-28 13:11:10 -0400

    Subiendo el proyecto Clientes-frontend

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'Andres',   apellido: 'Mamani', email: 'andres.guzman@gmail.com', createAt: '2020-12-11' },
+    { id: 2, nombre: 'Juan',     apellido: 'Nina', email: 'juan@gmail.com', createAt: '2020-12-11' },
+    { id: 3, nombre: 'Gabriel',  apellido: 'Salgado', email: 'gabriel@gmail.com', createAt: '2020-12-11' },
+    { id: 4, nombre: 'Maria',    apellido: 'Castro', email: 'maria@gmail.com', createAt: '2020-12-11' },
+    { id: 5, nombre: 'Lourdes',  apellido: 'Penaloza', email: 'lourdes@gmail.com', createAt: '2020-12-11' },
+    { id: 6, nombre: 'Andrea',   apellido: 'Flores', email: 'andrea@gmail.com', createAt: '2020-12-11' },
+    { id: 7, nombre: 'Reynaldo', apellido: 'Bellot', email: 'reynaldo@gmail.com', createAt: '2020-12-11' },
+    { id: 8, nombre: 'Alejandro', apellido: 'Osorio', email: 'alejandro@gmail.com', createAt: '2020-12-11' },
+    { id: 9, nombre: 'Gloria',   apellido: 'Guzman', email: 'gloria@gmail.com', createAt: '2020-12-11' },
+  ];
---
+++ src/app/header/header.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Header } from './header.component.ts/header.component.ts.component';
+
+
+
+@NgModule({
+  declarations: [
+    Header.Component.TsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
